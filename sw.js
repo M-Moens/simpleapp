37d948170cccfe0035352363b2db6dcf8ff410e4
M-Moens <107762955+M-Moens@users.noreply.github.com>
2025-09-20 22:30:42 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+// Basic offline cache for PWA
+const CACHE = "two-pane-notes-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  const { request } = e;
+  // Network-first for html, cache-first for static
+  if (request.mode === "navigate" || request.destination === "document") {
+    e.respondWith(
+      fetch(request).catch(() => caches.match("./index.html"))
+    );
+  } else {
+    e.respondWith(
+      caches.match(request).then(res => res || fetch(request))
+    );
+  }
+});
